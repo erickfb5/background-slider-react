8448a7480987ce1023e055e8f4433bd70f73b5a7
Erick Brandao <brandaoerick@MacBook-Air.local>
2023-01-25 18:40:16 -0300

    updated App.js

--- background-slider-react/src/App.js
+++ background-slider-react/src/App.js
@@ -1,11 +1,46 @@
+import { useState, useEffect } from "react";
 
+import { slides } from "./slides";
 import "./App.css";
 
 const App = () => {
+  const [activeSlide, setActiveSlide] = useState(0);
+
+  useEffect(() => {
+    document.body.style.backgroundImage = `url(${slides[activeSlide]})`;
+  }, [activeSlide, slides]);
+
+  const handleClick = (direction) => {
+    setActiveSlide(
+      direction === "right"
+        ? (activeSlide + 1) % slides.length
+        : (activeSlide - 1 + slides.length) % slides.length
+    );
+  };
   return (
-    <>
-      <div></div>
-    </>
+    <div className="slider-container">
+      {slides.map((slide, index) => (
+        <div
+          key={index}
+          className={`slide ${index === activeSlide ? "active" : ""}`}
+          style={{ backgroundImage: `url(${slide})` }}
+        />
+      ))}
+      <button
+        className="arrow left-arrow"
+        id="left"
+        onClick={() => handleClick("left")}
+      >
+        <i className="fas fa-arrow-left" />
+      </button>
+      <button
+        className="arrow right-arrow"
+        id="right"
+        onClick={() => handleClick("right")}
+      >
+        <i className="fas fa-arrow-right" />
+      </button>
+    </div>
   );
 };
 
